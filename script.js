eec9f29bec254185cfb4c0c662b8fa84a4d3f472
maedoml <maedoml02@gmail.com>
2025-10-23 17:24:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const products = [
   { id: 23, name: "Tênis NIKE Air", price: 80, image: "img/23.png", description: "Nº26 ao Nº33." },
   { id: 24, name: "Tênis NIKE", price: 99, image: "img/24.png", description: "N°." },
   { id: 25, name: "Tênis NIKE", price: 80, image: "img/25.png", description: "N°." },
-  { id: 26, name: "Tênis ALL STAR", price: 99.90, image: "img/26.png", description: "N°." },
+  { id: 26, name: "Tênis ALL STAR", price: 99, image: "img/26.png", description: "N°." },
   { id: 27, name: "Tênis ADIDAS", price: 190, image: "img/27.png", description: "N°." },
   { id: 28, name: "Tênis ADIDAS", price: 80, image: "img/28.png", description: "N°." },
   { id: 29, name: "Tênis NIKE", price: 80, image: "img/29.png", description: "N°." },
@@ -237,6 +237,7 @@ window.payWithCard = payWithCard;
 window.payOnDelivery = payOnDelivery;
 window.checkoutWhatsApp = checkoutWhatsApp;
 window.scrollToTop = scrollToTop;
+
 
 
 
